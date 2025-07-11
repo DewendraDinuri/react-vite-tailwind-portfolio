1d0bd51ad6a76e67686aa1dc4c0aebcb1d98287c
DINURI BUDHARA DEWENDRA <85922443+DewendraDinuri@users.noreply.github.com>
2025-07-11 22:55:38 +0530

    Crete new file call hero.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import React from 'react';
 import Navbar from './components/Navbar.jsx';
 
- const App = () => {
+const App = () => {
   return (
-    <div>
-      <Navbar/> 
-      <div className="relative min-h-screen w-full bg-slate-950 overflow-hidden">
+    <div className="min-h-screen w-full bg-slate-950 overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
+      
+      <div className="fixed top-0 -z-10 h-full w-full"></div>
+      {/* Gradient Background Bubbles */}
+      <div className="relative w-full h-full overflow-hidden">
+        {/* Left Gradient */}
         <div className="absolute top-[-10%] left-[-20%] h-[500px] w-[500px] rounded-full" style={{ background: 'radial-gradient(circle farthest-side, rgba(255, 0, 182, 0.15), rgba(255, 255, 255, 0))',}}></div>
-        <div className="absolute top-[-10%] right-[-20%] h-[500px] w-[500px] rounded-full" style={{ background: 'radial-gradient(circle farthest-side, rgba(255, 0, 182, 0.15), rgba(255, 255, 255, 0))', }}></div>
+        {/* Right Gradient */}
+        <div className="absolute top-[-10%] right-[-20%] h-[500px] w-[500px] rounded-full" style={{ background: 'radial-gradient(circle farthest-side, rgba(255, 0, 182, 0.15), rgba(255, 255, 255, 0))',}}></div>
       </div>
+      <div className="container mx-auto px-8">
+
+      </div>
+      <Navbar/>
     </div>
   );
 };
 
-export default App
+export default App;
