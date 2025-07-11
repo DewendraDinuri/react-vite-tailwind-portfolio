0b279b1e1c5f214b0ebcc57ec055d9da6b88cc1f
DINURI BUDHARA DEWENDRA <85922443+DewendraDinuri@users.noreply.github.com>
2025-07-11 12:24:31 +0530

    Update  Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,11 @@ import { FaLinkedin, FaGithub, FaInstagram, FaYoutube } from 'react-icons/fa';
 
 const Navbar = () => {
   return (
-    <nav className="fixed top-0 left-0 w-full z-50 flex items-center justify-between py-6 px-8 bg-white shadow">
+    <nav className="bg-red-900 flex items-center justify-between py-4 px-8 shadow-lg">
       <div className="flex flex-shrink-0 items-center">
         <img src={logo} alt="Logo" className="h-10 w-auto" />
       </div>
-      <div className="flex items-center justify-center gap-4 text-2xl">
+      <div className="flex items-center justify-center gap-8 text-2xl">
         <FaLinkedin/>
         <FaGithub/>
         <FaYoutube/>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
  /** @type {import('tailwindcss').Config} */
-export default {
-   content: [
-    "./index.html",
-    "./src/**/*.{html,js}"
-  ],
-   theme: {
-     extend: {},
-   },
-   plugins: [],
- }
+export const content = [
+  "./index.html",
+  "./src/**/*.{js,ts,jsx,tsx}",
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
