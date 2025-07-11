57218f095d78acaf2bfcb9f048893bae1a96e31a
DINURI BUDHARA DEWENDRA <85922443+DewendraDinuri@users.noreply.github.com>
2025-07-11 13:02:26 +0530

    add new background color
    <Navbar/>
          <div className="relative min-h-screen w-full bg-slate-950 overflow-hidden">
            <div className="absolute top-[-10%] left-[-20%] h-[500px] w-[500px] rounded-full" style={{ background: 'radial-gradient(circle farthest-side, rgba(255, 0, 182, 0.15), rgba(255, 255, 255, 0))',}}></div>
            <div className="absolute top-[-10%] right-[-20%] h-[500px] w-[500px] rounded-full" style={{ background: 'radial-gradient(circle farthest-side, rgba(255, 0, 182, 0.15), rgba(255, 255, 255, 0))', }}></div>
          </div>

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,11 @@ import Navbar from './components/Navbar.jsx';
  const App = () => {
   return (
     <div>
-      <div className="relative h-full w-full bg-slate-950"><div className="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div><div className ="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div></div>
       <Navbar/> 
+      <div className="relative min-h-screen w-full bg-slate-950 overflow-hidden">
+        <div className="absolute top-[-10%] left-[-20%] h-[500px] w-[500px] rounded-full" style={{ background: 'radial-gradient(circle farthest-side, rgba(255, 0, 182, 0.15), rgba(255, 255, 255, 0))',}}></div>
+        <div className="absolute top-[-10%] right-[-20%] h-[500px] w-[500px] rounded-full" style={{ background: 'radial-gradient(circle farthest-side, rgba(255, 0, 182, 0.15), rgba(255, 255, 255, 0))', }}></div>
+      </div>
     </div>
   );
 };
