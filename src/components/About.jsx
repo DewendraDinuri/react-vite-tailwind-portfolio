b21c21b33c156e4f0d4c6148be70cad57e942006
DINURI BUDHARA DEWENDRA <85922443+DewendraDinuri@users.noreply.github.com>
2025-07-13 12:01:24 +0530

    Update the Logo
    Add my LOGO

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,7 @@
 const About = () => {
   return (
-    <div>
-      about
+    <div className="border-b-0 border-neutral-900 pb-4 lg:mb-35">
+      <h2 className="my-20 text-center">About Me</h2>
     </div>
   )
 }
