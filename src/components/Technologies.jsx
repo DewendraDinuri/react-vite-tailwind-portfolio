e0d6bf301ca57554ce249f174ba3ed40485e32d4
DINURI BUDHARA DEWENDRA <85922443+DewendraDinuri@users.noreply.github.com>
2025-07-13 15:45:56 +0530

    Update Technologies.jsx

---
+++ src/components/Technologies.jsx
@@ -0,0 +1,13 @@
+
+const Technologies =()=> {
+  return (
+    <div className="border-b-0 border-neutral-800 pb-24 lg:mb-36 lg:text-3xl">
+        <h2>Technologies
+    
+        </h2>
+      
+    </div>
+  )
+};
+
+export default Technologies
