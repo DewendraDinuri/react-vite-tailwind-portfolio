5a13ab96feec0cb980650405f0d3e4d1b23e2196
DINURI BUDHARA DEWENDRA <85922443+DewendraDinuri@users.noreply.github.com>
2025-07-13 11:42:42 +0530

    Update Hero.jsx,index,js and About.jsx
    Create new file called About.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Navbar from './components/Navbar.jsx';
 import Hero from './components/Hero.jsx';
+import About from './components/About.jsx';
 
 const App = () => {
   return (
-    <div className="min-h-screen w-full bg-slate-950 overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
+    <div className="min-h-screen w-full bg-slate-950 overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900 px-16">
       
       <div className="fixed top-0 -z-10 h-full w-full"></div>
       {/* Gradient Background Bubbles */}
@@ -16,8 +17,9 @@ const App = () => {
       </div>
       <div className="container mx-auto px-8">
       </div>
-      <Navbar/>
-      <Hero/>
+      <Navbar />
+      <Hero />
+      <About />
     </div>
   );
 };
