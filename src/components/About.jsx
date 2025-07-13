004d57145b02dc7fa93d378fee83e9807f12cf0b
DINURI BUDHARA DEWENDRA <85922443+DewendraDinuri@users.noreply.github.com>
2025-07-13 12:25:17 +0530

    Update about.jsx
    change the font size of the about me text to 3xl

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,8 @@
+import aboutImg from "../assets/about.jpg";
 const About = () => {
   return (
-    <div className="border-b-0 border-neutral-900 pb-4 lg:mb-35">
-      <h2 className="my-20 text-center">About 
+    <div className="border-b-0 border-neutral-900 pb-4 lg:mb-35 lg:text-3xl">
+      <h2 className="my-24 text-center">About 
          <span className="text-neutral-500"> Me</span></h2>
          <div className="flex flex-wrap">
             <div className="w-full lg:w-1/2 lg:p-8">
