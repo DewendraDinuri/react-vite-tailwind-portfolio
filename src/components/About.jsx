796ac109760e9cb88738272c23623ed3ab46f315
DINURI BUDHARA DEWENDRA <85922443+DewendraDinuri@users.noreply.github.com>
2025-07-13 12:20:09 +0530

    Update About.jsx
    Update About section
    
    <span> me </Span>
    
    & add about image

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,15 @@
 const About = () => {
   return (
     <div className="border-b-0 border-neutral-900 pb-4 lg:mb-35">
-      <h2 className="my-20 text-center">About Me</h2>
+      <h2 className="my-20 text-center">About 
+         <span className="text-neutral-500"> Me</span></h2>
+         <div className="flex flex-wrap">
+            <div className="w-full lg:w-1/2 lg:p-8">
+            <div className="flex items-center justify-center">
+                <img src={aboutImg} alt="About Image" />
+            </div>
+            </div>
+         </div>
     </div>
   )
 }
