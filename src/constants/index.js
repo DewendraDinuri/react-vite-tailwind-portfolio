9a882ea4038af91b744125d0b7f0225810946596
DINURI BUDHARA DEWENDRA <85922443+DewendraDinuri@users.noreply.github.com>
2025-07-18 23:03:20 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -39,28 +39,28 @@ export const EXPERIENCES = [
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    title: "Agri Adapt - AI Powered Crop Management",
     image: project1,
     description:
       "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+    technologies: ["Flutter", "React", "Node.js", "Firebase", "MongoDB"],
   },
   {
-    title: "Task Management App",
+    title: "Real-Time Event Ticketing System",
     image: project2,
     description:
       "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+    technologies: ["JAVA", "React", "Node.js", "NOSQL"],
   },
   {
     title: "Portfolio Website",
     image: project3,
     description:
       "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+    technologies: ["React", "Tailwindcss"],
   },
   {
-    title: "Blogging Platform",
+    title: "Blogging Platform", 
     image: project4,
     description:
       "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
