e8d5f1181507994db9c63a700cdaf9e4d621ff21
DINURI BUDHARA DEWENDRA <85922443+DewendraDinuri@users.noreply.github.com>
2025-07-21 23:07:21 +0530

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
       </h2>
 
       <div className="flex flex-wrap">
-        {/* Image Section */}
+        {/* Image Section */ }
         <div className="w-full lg:w-1/2 lg:p-8">
           <div className="flex items-center justify-center">
             <img
