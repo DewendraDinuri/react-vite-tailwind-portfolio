a523f82130ff51646323339488a50180c5c4dbf0
DINURI BUDHARA DEWENDRA <85922443+DewendraDinuri@users.noreply.github.com>
2025-08-30 21:52:13 +0530

    update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,30 +1,63 @@
-import { HERO_CONTENT } from "../constants"
-import porfilePic from "../assets/kevinRushProfile.png"
-import {motion} from "framer-motion"
+import { HERO_CONTENT } from "../constants";
+import profilePic from "../assets/kevinRushProfile.png";
+import { delay, motion } from "framer-motion";
+import { MdDisabledVisible } from "react-icons/md";
+
+const container = (delay) => ({
+  hidden: { x: -100, opacity: 0 },
+  visible: { 
+    x: 0, 
+    opacity: 1, 
+    transition: { type: "spring", stiffness: 50, delay: delay } 
+  }
+});
+
 
 const Hero = () => {
-  return  <div className="border-b-0 border-neutral-900 pb-4 lg:mb-35">
-    <div className="flex flex-wrap">
+  return (
+    <div className="border-b-0 border-neutral-900 pb-4 lg:mb-36">
+      <div className="flex flex-wrap">
+        {/* Left Side - Text */}
         <div className="w-full lg:w-1/2">
-            <div className="flex flex-col items-center lg:items-start">
-                <motion.h1 initial={{x:-100, opacity: 0}}
-                animate ={{x:0, opacity: 1}} 
-                transition={{duration: 0.5, delay: 0.5}} 
-                className="pb-16 text-6xl font-thin tracking-tight lg:mt-32 lg:text-7xl text-white px-16">Dinza Dewen
-                </motion.h1>
-                <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent px-16">Software Engineer</span>
-                <p className="my-2 max-w-l py-6 font-light tracking-tighter text-justify px-16">{HERO_CONTENT}</p>
-            </div>
+          <div className="flex flex-col items-center lg:items-start">
+            <motion.h1
+              variants={container(0)}
+              initial="hidden"
+              animate="visible"
+              className="pb-16 text-6xl font-thin tracking-tight lg:mt-32 lg:text-7xl text-white px-16"
+            >
+              Dinza Dewen
+            </motion.h1>
+            
+            <motion.span 
+            variants={container(0.5)}
+              initial="hidden"
+              animate="visible"
+            className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent px-16">
+              Software Engineer
+            </motion.span>
+            
+            <motion.p 
+            variants={container(1)}
+              initial="hidden"
+              animate="visible"
+            className="my-2 max-w-lg py-6 font-light tracking-tighter text-justify px-16">
+              {HERO_CONTENT}
+            </motion.p>
+          </div>
         </div>
-        <div className="w-full lg:w-1/2 lg:p-8 flex justify-center lg:justify-start">
+
+        {/* Right Side - Image */}
+        <motion.div className="w-full lg:w-1/2 lg:p-8 flex justify-center lg:justify-start">
           <img
-            src={porfilePic}
+            src={profilePic}
             alt="Profile Picture"
             className="max-w-[280px] md:max-w-[350px] lg:max-w-[400px] ml-24 w-full h-auto rounded-2xl shadow-lg object-cover transform -scale-x-100"
           />
-        </div>
+        </motion.ddiv>
+      </div>
     </div>
-  </div>
-}
+  );
+};
 
-export default Hero
+export default Hero;
